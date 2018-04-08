29f337328f9ec82964db26cd47842dac94d8ed03
Gonzalo Barra <gonzalobarra@MacBook-Air-de-Gonzalo.local>
2018-04-08 20:03:01 -0300

    commit inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import {HomeModule} from './modules/home/home.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/public_api.ts
@@ -0,0 +1,1 @@
+export * from './app/modules/home/home.module';
